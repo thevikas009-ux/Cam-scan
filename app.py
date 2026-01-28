2c7be9a6c4d47462101b729fcd74a6bf1a3c4861
thevikas009-ux <thevikas009@gmail.com>
2026-01-28 15:46:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Image to Google Sheet", layout="centered")
 col1, col2 = st.columns([1, 4])
 
 with col1:
-    st.image("logo.png", width=90)
+    st.image("logo.png", width=120)
 
 with col2:
     st.markdown(
